23558de6fe45b8c065d778e4778526829de1f30b
Kalcn <zoon3312@gmai.com>
2020-05-07 13:23:53 +0800

    更新程式內容

--- app/src/main/java/com/kalcn/radioapp/MainActivity.java
+++ app/src/main/java/com/kalcn/radioapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.kalcn.radioapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        TextView textView2 = findViewById(R.id.textView2);
+        RadioGroup Group = (RadioGroup)findViewById(R.id.Group);
+
+        int id = Group.getCheckedRadioButtonId();
+        RadioButton selected = (RadioButton)findViewById(id);
+
+        textView2.setText("我喜歡的是："+selected.getText());
+
     }
 }
